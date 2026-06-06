918a52d8c9cf7ef70032383ef799b735986f786e
Andrew <andrew_hulme04@hotmail.com>
2026-06-06 12:38:06 +1000

    Fixed a bug where signing out while in the social page resulted in the app crashing. I made the sign out button also redirect back to the home page to prevent this, but will implement handling missing sessions later to make everything more robust

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -57,7 +57,7 @@ export function Navbar() {
 
                     {session?.user ? (
                         <button
-                            onClick={() => signOut()}
+                            onClick={() => signOut({ callbackUrl: "/" })}
                             className="rounded-lg border border-zinc-700 px-4 py-2 text-sm font-semibold text-zinc-300 hover:bg-zinc-900"
                             >
                             Sign out
