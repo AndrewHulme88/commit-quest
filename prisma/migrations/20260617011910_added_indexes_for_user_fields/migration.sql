2015166b3c3316f4d731aa1ce78465234ec866d3
Andrew <andrew_hulme04@hotmail.com>
2026-06-17 11:20:58 +1000

    Added database indexes to User model to fields used by the leaderboard. This will help with scalability the more users the app has.

---
+++ prisma/migrations/20260617011910_added_indexes_for_user_fields/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "User_xp_idx" ON "User"("xp");
+
+-- CreateIndex
+CREATE INDEX "User_level_idx" ON "User"("level");
+
+-- CreateIndex
+CREATE INDEX "User_streak_idx" ON "User"("streak");
+
+-- CreateIndex
+CREATE INDEX "User_highest_streak_idx" ON "User"("highest_streak");
