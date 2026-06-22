cbdc3ff5f0818ea7d138635f597255b725db5635
Andrew <andrew_hulme04@hotmail.com>
2026-06-22 16:24:19 +1000

    Added Activity model to database to set up for adding the activity feed

---
+++ prisma/migrations/20260622062323_add_activity_feed/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Activity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Activity_createdAt_idx" ON "Activity"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Activity_userId_idx" ON "Activity"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
