925fcdf5e591833019dafb111b573e0a5a16b3a4
Andrew <andrew_hulme04@hotmail.com>
2026-06-23 11:14:06 +1000

    Added cascade delete to all the user relations in the database to correctly handle when a user deletes their account

---
+++ prisma/migrations/20260623010946_add_cascade_delete/migration.sql
@@ -0,0 +1,29 @@
+-- DropForeignKey
+ALTER TABLE "Activity" DROP CONSTRAINT "Activity_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_followerId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_followingId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "GithubEvent" DROP CONSTRAINT "GithubEvent_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserAchievement" DROP CONSTRAINT "UserAchievement_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "GithubEvent" ADD CONSTRAINT "GithubEvent_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follow" ADD CONSTRAINT "Follow_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserAchievement" ADD CONSTRAINT "UserAchievement_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
